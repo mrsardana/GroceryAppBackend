e9bb1b4ce5b919a65a6da3b9f55347acd9e21c68
Deepak Sardana <deepaksardana03@gmail.com>
2023-03-21 19:22:19 -0400

    added .env

---
+++ config/app.config.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+console.log("app.config" + process.env.URL)
+const MONGO_DB_CONFIG = {
+    // DB: "mongodb://127.0.0.1:27017/grocery-app-db",
+    DB: process.env.URL,
+    PAGE_SIZE: 10,
+};
+module.exports = {
+    MONGO_DB_CONFIG,
+};
---
+++ config/firebase.config.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { initializeApp } = require('firebase/app');
+const { getFirestore } = require('firebase/firestore/lite');
+
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGE_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASURMENT_ID
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+module.exports = db;
